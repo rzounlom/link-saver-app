2af05f535e064fc2c2e4e7080e6be535fb4c719b
rzounlom <rzounlom@gmail.com>
2025-03-27 05:55:08 -0400

    file clean up

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import { auth } from "@clerk/nextjs/server";
 
 const DashboardPage: FC = async () => {
   const { userId } = await auth();
-  console.log({ userId });
 
   if (!userId) return null; // Will redirect using middleware
   return <div className="p-4">Welcome to your dashboard!</div>;
